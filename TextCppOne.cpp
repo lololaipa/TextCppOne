010a31b4141660ba19b962dca21e409a84d3dc3a
lololaipa <lololaipa@gmail.com>
2026-05-31 10:12:38 +0300

    Додала масив і ввела його з консолі

--- TextCppOne.cpp
+++ TextCppOne.cpp
@@ -7,4 +7,10 @@ int main (){
 
     cout << "Good Morning" << endl;
     return 0;
+
+    char name[80];
+    cout << "Як Вас звати?\n";
+    cout << "->_";
+    cin.getline(name, 80, '\n');
+    cout << "Приємно познайомитися " << name << " :)\n";
 }
